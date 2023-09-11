106def09348be2076f4c8ced89b821ea0018ab90
DenysBorozenets <borozenetsdenysforme@gmail.com>
2023-09-12 00:41:23 +0300

    Registration and login are complete

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User.js');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
 
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'asdfghjklwskjfidnwmkwidnbejdlclc';
+
 app.use(express.json());
+app.use(cookieParser());
+
 app.use(
     cors({
         credentials: true,
@@ -10,13 +23,63 @@ app.use(
     })
 );
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
-    res.json({ name, email, password });
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+        const passCorect = bcrypt.compareSync(password, userDoc.password);
+        if (passCorect) {
+            jwt.sign(
+                { email: userDoc.email, id: userDoc._id },
+                jwtSecret,
+                {},
+                (err, token) => {
+                    if (err) throw err;
+                    res.cookie('token', token, {
+                        sameSite: 'none',
+                        secure: 'true',
+                    }).json(userDoc);
+                }
+            );
+        } else {
+            res.status(422).json('password incorrect');
+        }
+    } else {
+        res.json('not found');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err;
+            const { name, email, _id } = await User.findById(userData.id);
+            res.json({ name, email, _id });
+        });
+    } else {
+        res.json(null);
+    }
 });
 
 app.listen(4000);
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from './UserContext.jsx';
 
 export default function Header() {
+    const { user } = useContext(UserContext);
     return (
         <header className="flex justify-between">
             <a href="" className="flex items-center gap-1">
@@ -70,6 +73,7 @@ export default function Header() {
                         />
                     </svg>
                 </div>
+                {!!user && <div>{user.name}</div>}
             </Link>
         </header>
     );
